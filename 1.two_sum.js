7900c96d150e7d59035d6b2cfe8c45b2b2b3310e
AFSAL333 <afsalafs8008@gmail.com>
2024-06-12 21:12:44 +0530

    1

---
+++ 1.two_sum.js
@@ -0,0 +1,23 @@
+function twoSum(nums, target) {
+    let map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    
+    // If no solution is found (though the problem guarantees one)
+    throw new Error("No two sum solution");
+}
+
+// Example 1
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
+
+// Example 2
+console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
+
+// Example 3
+console.log(twoSum([3, 3], 6)); // Output: [0, 1]
