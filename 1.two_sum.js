5ebd5c094bfdf8c43520f2e6c890a1e8f0bfcfa5
AFSAL333 <afsalafs8008@gmail.com>
2024-06-13 19:35:59 +0530

    1

--- 1.two_sum.js
+++ 1.two_sum.js
@@ -1,12 +1,12 @@
-function twoSum(nums, target) {
+function twoSum(numbers, target) {
     let map = new Map();
     
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
+    for (let i = 0; i < numbers.length; i++) {
+        let complement = target - numbers[i];
         if (map.has(complement)) {
             return [map.get(complement), i];
         }
-        map.set(nums[i], i);
+        map.set(numbers[i], i);
     }
     
     // If no solution is found (though the problem guarantees one)
