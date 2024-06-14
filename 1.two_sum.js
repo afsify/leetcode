993232870d6fff8b558f4d45e4145f3082409b5d
AFSAL333 <afsalafs8008@gmail.com>
2024-06-14 19:23:09 +0530

    1

--- 1.two_sum.js
+++ 1.two_sum.js
@@ -1,16 +1,16 @@
 function twoSum(numbers, target) {
-    let map = new Map();
-    
-    for (let i = 0; i < numbers.length; i++) {
-        let complement = target - numbers[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-        }
-        map.set(numbers[i], i);
+  let map = new Map();
+
+  for (let i = 0; i < numbers.length; i++) {
+    let complement = target - numbers[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
     }
-    
-    // If no solution is found (though the problem guarantees one)
-    throw new Error("No two sum solution");
+    map.set(numbers[i], i);
+  }
+
+  // If no solution is found (though the problem guarantees one)
+  throw new Error("No two sum solution");
 }
 
 // Example 1
