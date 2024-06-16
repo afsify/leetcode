11f5653726fd16d9b2aa6d161640548a24ea94e4
AFSAL333 <afsalafs8008@gmail.com>
2024-06-16 11:46:59 +0530

    1

--- 1.two_sum.js
+++ 1.two_sum.js
@@ -13,11 +13,7 @@ function twoSum(numbers, target) {
   throw new Error("No two sum solution");
 }
 
-// Example 1
+// Example usage
 console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
-
-// Example 2
 console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
-
-// Example 3
 console.log(twoSum([3, 3], 6)); // Output: [0, 1]
