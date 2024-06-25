1d538dccaaacc1530696aa0f26b65c857b3b0546
AFSAL333 <afsalafs8008@gmail.com>
2024-06-25 17:40:02 +0530

    2769

---
+++ 2769.achievable_number.js
@@ -0,0 +1,14 @@
+function maxAchievableNumber(num, t) {
+  // The maximum achievable number is obtained by adding twice the number of operations (t) to the original number
+  return num + 2 * t;
+}
+
+// Example 1
+let num1 = 4;
+let t1 = 1;
+console.log(maxAchievableNumber(num1, t1)); // Output: 6
+
+// Example 2
+let num2 = 3;
+let t2 = 2;
+console.log(maxAchievableNumber(num2, t2)); // Output: 7
