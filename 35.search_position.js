e5673a8a91c44085eb913c8763fc2656652a6cbb
AFSAL333 <afsalafs8008@gmail.com>
2024-06-30 23:01:51 +0530

    35

---
+++ 35.search_position.js
@@ -0,0 +1,23 @@
+function searchInsert(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+}
+
+// Example usage:
+console.log(searchInsert([1, 3, 5, 6], 5)); // Output: 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // Output: 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // Output: 4
